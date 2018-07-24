44d97609f0af99211e35b886d43ac6c41e08618a
octocodeio <wojciech.dasiukiewicz@gmail.com>
2018-07-24 11:50:22 +0200

    Bootstrap change

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import ReactDOM from "react-dom";
 import React from "react";
 import { App } from "./components/app/App.jsx";
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+
 ReactDOM.render(
     <App />,
     document.getElementById("mountPoint")
